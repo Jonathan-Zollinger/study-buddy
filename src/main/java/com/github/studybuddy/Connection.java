60f06ee1da6f3c0962e441c7604a43a619ec7ab0
jonathan zollinger <jonathan.zollinger@outlook.com>
2023-08-13 23:44:52 -0600

    build!: remove mongo, add dgraph

---
+++ src/main/java/com/github/studybuddy/Connection.java
@@ -0,0 +1,15 @@
+package com.github;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Connection {
+
+    public static void main(String[] args) {
+
+    }
+}
