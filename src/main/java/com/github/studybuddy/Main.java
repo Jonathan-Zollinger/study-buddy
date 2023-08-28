afb68dbd2241a1952be0e0fce73dc3733fc64dec
Jonathan Zollinger <jonathan.zollinger@gmail.com>
2023-08-27 23:01:45 -0600

    wip(drop me): reading in api info from properties

--- src/main/java/com/github/studybuddy/Main.java
+++ src/main/java/com/github/studybuddy/Main.java
@@ -1,7 +1,19 @@
 package com.github.studybuddy;
 
+import com.github.studybuddy.utils.DgraphUtils;
+import io.dgraph.DgraphClient;
+import io.dgraph.DgraphProto;
+
+import java.util.ResourceBundle;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ResourceBundle properties = ResourceBundle.getBundle("config"); //put config.properties in resources
+
+        DgraphClient dgraphClient = DgraphUtils.initiateClient(properties.getString("dGraphEndpoint"),
+                properties.getString("bearerToken"));
+
+        DgraphProto.Version version = dgraphClient.checkVersion();
+        System.out.println(version);
     }
 }
--- src/main/java/com/github/studybuddy/utils/DgraphUtils.java
+++ src/main/java/com/github/studybuddy/utils/DgraphUtils.java
@@ -4,6 +4,8 @@ import io.dgraph.DgraphClient;
 import io.dgraph.DgraphGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.Metadata;
+import io.grpc.stub.MetadataUtils;
 
 public class DgraphUtils {
 
@@ -23,4 +25,18 @@ public class DgraphUtils {
     DgraphGrpc.DgraphStub stub3 = DgraphGrpc.newStub(channel3);
 
     DgraphClient dgraphClient = new DgraphClient(stub1, stub2, stub3);
+
+    public static DgraphClient initiateClient(String endpoint, String bearerToken) {
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(endpoint).useTransportSecurity().build();
+
+        // If you have an API key, you should attach it to all your requests.
+        Metadata metadata = new Metadata();
+        Metadata.Key<String> apiKeyHeader = Metadata.Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER);
+        metadata.put(apiKeyHeader, "Bearer <YOUR_API_KEY>"); // Replace <YOUR_API_KEY> with your actual API key
+
+        DgraphGrpc.DgraphStub stub = DgraphGrpc.newStub(channel);
+        stub = MetadataUtils.attachHeaders(stub, metadata);
+
+        return new DgraphClient(stub);
+    }
 }
