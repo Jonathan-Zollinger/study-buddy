a84a1b581765409bc4b2ca5e9c0883d026cbf793
Jonathan Zollinger <jonathan.zollinger@gmail.com>
2023-09-03 23:11:19 -0600

    feat wip: add standard works data classes

--- src/main/java/com/github/studybuddy/Main.java
+++ src/main/java/com/github/studybuddy/Main.java
@@ -1,9 +1,15 @@
 package com.github.studybuddy;
 
-import com.github.studybuddy.utils.DgraphUtils;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dgraph.DgraphClient;
-import io.dgraph.DgraphProto;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Main {
@@ -12,6 +18,43 @@ public class Main {
 
         DgraphClient dgraphClient = DgraphUtils.initiateClient(properties.getString("dGraphEndpoint"),
                 properties.getString("bearerToken"));
+        System.out.println(importStandardWorks());
+    }
+
+    static DgraphClient getDgraphClient(String endpoint, String bearerToken) throws MalformedURLException {
+        return new DgraphClient(DgraphClient.clientStubFromSlashEndpoint(endpoint, bearerToken));
+    }
+
+    static List<Volume> importStandardWorks() throws IOException {
+        return new ObjectMapper().readValue(new File("src/main/resources/lds-scriptures-2020.12.08/json/kjv-scriptures-json.txt".replace("/", File.separator)), new TypeReference<List<Volume>>(){});
+    }
+
+    @Data
+    @NoArgsConstructor
+        private class Volume {
+        private String id;
+        private String title;
+        private List<Book> books;
+    }
+
+    @Data
+    @NoArgsConstructor
+        private class Book {
+        private String id;
+        private String title;
+        private String shortTitle;
+        private Volume volume;
+        private List<Chapter> chapters;
+    }
+
+    @Data
+    @NoArgsConstructor
+        private class Chapter {
+        private String id;
+        private int number;
+        private Book book;
+        private List<Verse> verses;
+    }
 
         DgraphProto.Version version = dgraphClient.checkVersion();
         System.out.println(version);
